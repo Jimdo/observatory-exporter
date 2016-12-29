b83cf1058d05c4cbb02ef8b8fc3ee27883f2c9ef
Simon Hartmann <simon.hartmann@jimdo.com>
2016-12-29 16:43:00 +0100

    add missing namespace for metrics desc

--- exporter.go
+++ exporter.go
@@ -24,12 +24,12 @@ func NewExporter(targetURL string, fetch func() *Metrics) *Exporter {
 		targetURL: targetURL,
 		fetch:     fetch,
 		metrics: map[string]*prometheus.Desc{
-			"tls_enabled": prometheus.NewDesc("tls_enabled", "TLS enabled for domain", nil, targetLabel),
-			"is_valid":    prometheus.NewDesc("is_valid", "Is 1 (aka 'valid') if any of the per truststore valitities is valid", nil, targetLabel),
-			"ssl_level":   prometheus.NewDesc("ssl_level", "Defines the Mozilla SSL level for given domain (old=0, intermediate=1, modern=2)", nil, targetLabel),
-			"score":       prometheus.NewDesc("score", "Defines the score given by Mozilla Observatory's mozillaGradingWorker (0...100)", nil, targetLabel),
-			"grade":       prometheus.NewDesc("grade", "Grade representation of score, A=4, B=3, C=2, D=1, F=0", nil, targetLabel),
-			"expiry_date": prometheus.NewDesc("expiry_date", "Expiry date for certificate.", nil, targetLabel),
+			"tls_enabled":      prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "tls_enabled"), "TLS enabled for domain", nil, targetLabel),
+			"is_valid":         prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "is_valid"), "Is 1 (aka 'valid') if any of the per truststore valitities is valid", nil, targetLabel),
+			"ssl_level":        prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "ssl_level"), "Defines the Mozilla SSL level for given domain (old=0, intermediate=1, modern=2)", nil, targetLabel),
+			"score":            prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "score"), "Defines the score given by Mozilla Observatory's mozillaGradingWorker (0...100)", nil, targetLabel),
+			"grade":            prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "grade"), "Grade representation of score, A=4, B=3, C=2, D=1, F=0", nil, targetLabel),
+			"cert_expiry_date": prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "cert_expiry_date"), "Expiry date for certificate.", nil, targetLabel),
 		},
 	}
 	return &e
